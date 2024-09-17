1166c044851b20f187d70a79b7ab3978e1b5a84e
Bruno Soares <85403673+felipebss1@users.noreply.github.com>
2024-09-17 12:46:06 -0300

    Removed print statement in didChangeAppLifecycleState and updated dependencies to latest versions

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,22 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [];
 
   bool _showChart = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where(
@@ -142,7 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 recentTransactions: _recentTransactions,
               ),
             )
-          : txListWidget 
+          : txListWidget
     ];
   }
 
